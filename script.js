12dfd54563b226bcb1b6a0946c03ec3689fa86ad
Leandro Arturi <lea.arturi@gmail.com>
2023-05-11 23:49:27 -0300

    Mejora

--- script.js
+++ script.js
@@ -1,80 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-   setImagesTabsOnInit();
+   const tabs = document.querySelectorAll('.tab');
 
-   function setTabActiveImageAttributes(tab) {
-      // Recibe un tab y le coloca la imagen activa
+   tabs.forEach((tab) => {
       const imageEl = tab.querySelector('img');
-      if (imageEl) {
-         const imageActivePath = imageEl.getAttribute('data-active-tab-path');
-         if (imageActivePath) {
+
+      const imageDefaultPath = imageEl.getAttribute('data-default-path');
+      const imageActivePath = imageEl.getAttribute('data-active-tab-path');
+
+      if (tab.classList.contains('active')) {
+         imageEl.src = imageActivePath;
+      }
+
+      tab.addEventListener('mouseenter', () => {
+         if (!tab.classList.contains('active')) {
             imageEl.src = imageActivePath;
          }
-      }
-   }
+      });
+
+      tab.addEventListener('mouseleave', () => {
+         if (!tab.classList.contains('active')) {
+            imageEl.src = imageDefaultPath;
+         }
+      });
+
+      tab.addEventListener('click', () => {
+         tabs.forEach((ele) => {
+            ele.classList.remove('active');
+            setTabDefaultImageAttributes(ele);
+         });
+
+         tab.classList.add('active');
+         imageEl.src = imageActivePath;
+      });
+   });
 
    function setTabDefaultImageAttributes(tab) {
-      // Recibe un tab y le coloca la imagen default
       const imageEl = tab.querySelector('img');
       if (imageEl) {
          imageEl.src = imageEl.getAttribute('data-default-path');
       }
-   }
-
-   function setTabImageHoverBehavior(tab) {
-      // Cambia la imagen al hacer hover, excepto para tab activo
-      const imageEl = tab.querySelector('img');
-      if (imageEl) {
-         const imageHoverPath = imageEl.getAttribute('data-active-tab-path');
-         const imageDefaultPath = imageEl.getAttribute('data-default-path');
-
-         tab.addEventListener('mouseenter', () => {
-            const hasActiveClass = tab.classList.contains('active');
-            if (!hasActiveClass && imageHoverPath) {
-               imageEl.src = imageHoverPath;
-            }
-         });
-
-         tab.addEventListener('mouseleave', () => {
-            const hasActiveClass = tab.classList.contains('active');
-            if (!hasActiveClass) {
-               imageEl.src = imageDefaultPath;
-            }
-         });
-      }
-   }
-
-   function setImagesTabsOnInit() {
-      // Configuracion inicial de las imagenes de los tabs
-
-      const currentContentTab = document.querySelector('.tabs');
-      const tabs = currentContentTab.querySelectorAll('.tab');
-
-      tabs.forEach((tab) => {
-         const isTabActive = tab.classList.contains('active');
-
-         // Setea hover para todos los tabs
-         setTabImageHoverBehavior(tab);
-
-         // Setea tab activo
-         if (isTabActive) {
-            setTabActiveImageAttributes(tab);
-         }
-
-         // Setea click event para cada tab
-         tab.addEventListener('click', () => {
-            const selectedTab = tab;
-
-            tabs.forEach((ele) => {
-               // Setea hover y elimina class active para todos los tabs 
-               setTabImageHoverBehavior(ele);
-               ele.classList.remove('active');
-               setTabDefaultImageAttributes(ele);
-            });
-
-            // Setea el tab activo
-            selectedTab.classList.add('active');
-            setTabActiveImageAttributes(selectedTab);
-         });
-      });
    }
 });
